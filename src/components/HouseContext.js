034c305b353d45d2a4c76d4a0c53dca5db28b844
HimashaDewmini <himashadewmini12345@gmail.com>
2025-01-06 22:40:04 +0530

    updated

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -70,7 +70,7 @@ const HouseContextProvider = ({ children }) => {
         houses,
         loading,
         handleClick,
-        loading,
+        
       }}
     >
       {children}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 
 //import components
 import CountryDropdown from './CountryDropdown';
